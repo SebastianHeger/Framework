066a234a9a3e0ebddb3e5515584f6bce5a2a8a2d
SebastianHeger <sebastian@heger-tech.de>
2022-07-06 19:33:38 +0200

    adding enums and logger

---
+++ backend/enums.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class EnvironmentVariable(Enum):
+    LOG_LEVEL = "LOG_LEVEL"
---
+++ backend/utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+import os
+
+from enums import EnvironmentVariable
+
+log_level = os.getenv(EnvironmentVariable.LOG_LEVEL, "DEBUG").upper()
+
+logger = logging.getLogger("Backend")
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(log_level)
+console_handler.setFormatter(logging.Formatter(fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"))
+
+logger.addHandler(console_handler)
