e92f6490652f6f332782a7de93f46486fb7da79b
SebastianHeger <sebastian@heger-tech.de>
2022-07-13 19:55:14 +0200

    fix token refresh times

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -138,9 +138,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=3)
-    # 'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
 
 # Internationalization
